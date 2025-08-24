8b2c7c2016f29f673e53487baadcf9e325f801ce
Shikoz500 <mike45032@gmail.com>
2025-08-25 00:15:28 +0300

    SEO

---
+++ assets/js/Meta.js
@@ -0,0 +1,23 @@
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "QBoosting",
+    "alternateName": "QBoosting Store",
+    "url": "https://qboosting.store",
+    "logo": "https://qboosting.store/images/logo.png",
+    "description": "Professional FIFA 26 boosting service...",
+    "foundingDate": "2023",
+    "serviceArea": "Worldwide",
+    "contactPoint": {
+        "@type": "ContactPoint",
+        "telegram": "@QBoostingEAFC",
+        "contactType": "customer service"
+    },
+    "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "5",
+        "reviewCount": "3"
+    }
+}
+</script>
